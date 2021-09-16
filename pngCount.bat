4688de090b9287f091fed4ef42d827c22136bf4e
tkosaya <tkosaya@gmail.com>
2021-09-16 15:37:51 +0800

    Update pngCount.bat
    新增log

--- pngCount.bat
+++ pngCount.bat
@@ -3,15 +3,18 @@ setlocal EnableDelayedExpansion
 
 set pngFile=pngFile.txt
 set total=
-echo memory analyze>!pngFile!
+set pngCount=
 
+echo start 
+
+echo memory analyze>!pngFile!
 echo ====================>>!pngFile!
 
 for /R %%s in (*.jpg,*png) do (
 	set name=%%~ns
 	set wd=
 	set hi=
-	
+	echo !name!
 	for /f "delims=: tokens=1,2" %%i in ('mediainfo %%s') do (
 		set first=%%i
 		set sec=%%j
@@ -30,12 +33,16 @@ for /R %%s in (*.jpg,*png) do (
 	
 	set /a mem=wd*hi/1024*4
 	set /a total=total+mem
+	set /a pngCount=pngCount+1
 	echo FileName:!name!>>!pngFile!
 	echo Size:!wd! x !hi!>>!pngFile!
 	echo Memory:!mem!>>!pngFile!
 	echo ====================>>!pngFile!
 )
 
-echo Total:!total!>>!pngFile!
+echo end 
+
+echo Total pngCount:!pngCount!>>!pngFile!
+echo Total Memory:!total!>>!pngFile!
 
 pause
